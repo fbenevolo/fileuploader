423c832b6ced3d1da4cefb9a4d28f8aa330eea87
fbenevolo <benevolofelipe@gmail.com>
2026-06-27 16:32:51 -0300

    got back to custom routing in terraform

--- terraform/api_gateway.tf
+++ terraform/api_gateway.tf
@@ -13,35 +13,35 @@ resource "aws_apigatewayv2_integration" "lambda_integration" {
 
 # ======================== routes ========================
 
-# resource "aws_apigatewayv2_route" "list_route" {
-#   api_id = aws_apigatewayv2_api.fileuploader_api.id
-#   route_key = "GET /list"
-#   target = "integrations/${aws_apigatewayv2_integration.lambda_integration.id}"
-# }
+resource "aws_apigatewayv2_route" "list_route" {
+  api_id = aws_apigatewayv2_api.fileuploader_api.id
+  route_key = "GET /list"
+  target = "integrations/${aws_apigatewayv2_integration.lambda_integration.id}"
+}
 
-# resource "aws_apigatewayv2_route" "upload_route" {
-#   api_id = aws_apigatewayv2_api.fileuploader_api.id
-#   route_key = "POST /upload"
-#   target = "integrations/${aws_apigatewayv2_integration.lambda_integration.id}"
-# }
+resource "aws_apigatewayv2_route" "upload_route" {
+  api_id = aws_apigatewayv2_api.fileuploader_api.id
+  route_key = "POST /upload"
+  target = "integrations/${aws_apigatewayv2_integration.lambda_integration.id}"
+}
 
-# resource "aws_apigatewayv2_route" "download_route" {
-#   api_id = aws_apigatewayv2_api.fileuploader_api.id
-#   route_key = "GET /download/{stored_name}"
-#   target = "integrations/${aws_apigatewayv2_integration.lambda_integration.id}"
-# }
+resource "aws_apigatewayv2_route" "download_route" {
+  api_id = aws_apigatewayv2_api.fileuploader_api.id
+  route_key = "GET /download/{stored_name}"
+  target = "integrations/${aws_apigatewayv2_integration.lambda_integration.id}"
+}
 
 
-# resource "aws_apigatewayv2_route" "delete_route" {
-#   api_id = aws_apigatewayv2_api.fileuploader_api.id
-#   route_key = "DELETE /delete/{stored_name}"
-#   target = "integrations/${aws_apigatewayv2_integration.lambda_integration.id}"
-# }
+resource "aws_apigatewayv2_route" "delete_route" {
+  api_id = aws_apigatewayv2_api.fileuploader_api.id
+  route_key = "DELETE /delete/{stored_name}"
+  target = "integrations/${aws_apigatewayv2_integration.lambda_integration.id}"
+}
 
 # =========================================================
 
-resource "aws_apigatewayv2_route" "proxy" {
+resource "aws_apigatewayv2_stage" "default" {
   api_id = aws_apigatewayv2_api.fileuploader_api.id
-  route_key = "ANY /{proxy+}"
-  target    = "integrations/${aws_apigatewayv2_integration.lambda_integration.id}"
+  name = "$default"
+  auto_deploy = true
 }
