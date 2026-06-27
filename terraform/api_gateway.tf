dfb2fd715f8d23d755a875fcc8c91d9d79dae86d
fbenevolo <benevolofelipe@gmail.com>
2026-06-27 16:09:27 -0300

    testing with only one route in terraform api gateway

--- terraform/api_gateway.tf
+++ terraform/api_gateway.tf
@@ -13,28 +13,28 @@ resource "aws_apigatewayv2_integration" "lambda_integration" {
 
 # ======================== routes ========================
 
-resource "aws_apigatewayv2_route" "list_route" {
-  api_id = aws_apigatewayv2_api.fileuploader_api.id
-  route_key = "GET /list"
-  target = "integrations/${aws_apigatewayv2_integration.lambda_integration.id}"
-}
+# resource "aws_apigatewayv2_route" "list_route" {
+#   api_id = aws_apigatewayv2_api.fileuploader_api.id
+#   route_key = "GET /list"
+#   target = "integrations/${aws_apigatewayv2_integration.lambda_integration.id}"
+# }
 
-resource "aws_apigatewayv2_route" "upload_route" {
-  api_id = aws_apigatewayv2_api.fileuploader_api.id
-  route_key = "POST /upload"
-  target = "integrations/${aws_apigatewayv2_integration.lambda_integration.id}"
-}
+# resource "aws_apigatewayv2_route" "upload_route" {
+#   api_id = aws_apigatewayv2_api.fileuploader_api.id
+#   route_key = "POST /upload"
+#   target = "integrations/${aws_apigatewayv2_integration.lambda_integration.id}"
+# }
 
-resource "aws_apigatewayv2_route" "download_route" {
-  api_id = aws_apigatewayv2_api.fileuploader_api.id
-  route_key = "GET /downloads"
-  target = "integrations/${aws_apigatewayv2_integration.lambda_integration.id}"
-}
+# resource "aws_apigatewayv2_route" "download_route" {
+#   api_id = aws_apigatewayv2_api.fileuploader_api.id
+#   route_key = "GET /download/{stored_name}"
+#   target = "integrations/${aws_apigatewayv2_integration.lambda_integration.id}"
+# }
 
 
 resource "aws_apigatewayv2_route" "delete_route" {
   api_id = aws_apigatewayv2_api.fileuploader_api.id
-  route_key = "DELETE /delete"
+  route_key = "DELETE /delete/{stored_name}"
   target = "integrations/${aws_apigatewayv2_integration.lambda_integration.id}"
 }
 
